db6074ed284b6cf320b34bed07cb611849ebcde4
siraphat16 <109897886+siraphat16@users.noreply.github.com>
2022-11-27 23:05:18 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import All_csv from "./component/All_csv.js";
+
+function App() {
+  return (
+    <div className="App">
+      <All_csv/>
+    </div>
+  );
+}
+
+export default App;
